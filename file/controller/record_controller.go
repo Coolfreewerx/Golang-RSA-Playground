033da6cc56588617899924154899349c15cd3af0
Coolfreewerx <78583528+Coolfreewerx@users.noreply.github.com>
2023-08-28 17:25:16 +0700

    delete comment

--- file/controller/record_controller.go
+++ file/controller/record_controller.go
@@ -44,26 +44,3 @@ func (c *RecordController) GetRecordByNo(context echo.Context) error {
 	return context.JSON(http.StatusOK, records)
 
 }
-
-	// var records_bangkok []model.Record
- 
-	// Print the records
-	// for _, record := range records {
-	// 	if record.Province == "กรุงเทพมหานคร" {
-	// 		fmt.Printf("Name: %s, Sex: %s, Age: %d, Province: %s\n", record.Announce_date, record.Sex, record.Age, record.Province)
-	// 		records_bangkok = append(records_bangkok, record)
-	// 	}
-	// }
-
-	// total_of_woman, total_of_men := 0, 0
-
-	// for _, record_bankok := range records_bangkok {
-	// 	if record_bankok.Sex == "หญิง" {
-	// 		total_of_woman += 1
-	// 	} else if record_bankok.Sex == "ชาย" {
-	// 		total_of_men += 1
-	// 	}
-	// }
-	// fmt.Println()
-	// fmt.Println("จำนวนผู้ติดเชื้อในกรุงเทพมหานคร: ", len(records_bangkok), "ราย")
-	// fmt.Println("เป็นผู้หญิง: ", total_of_woman, "ราย", " เป็นผู้ชาย: ", total_of_men)

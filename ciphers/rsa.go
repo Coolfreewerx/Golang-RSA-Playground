2de9037d78cb7c9fa813cd24905af9b06955bc9d
Teerapat Kuiyawattananon <teerapat.ku@ku.th>
2023-08-30 10:46:27 +0700

    encrypt and decrypt file

--- ciphers/rsa.go
+++ ciphers/rsa.go
@@ -6,48 +6,50 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"hash"
+	"io"
 
 	"golang.org/x/crypto/openpgp"
 	"golang.org/x/crypto/openpgp/packet"
 )
 
 func ExportPublicKeyAsPemStr(pubkey *rsa.PublicKey) string {
-    pubkey_pem := string(pem.EncodeToMemory(&pem.Block{Type:  "RSA PUBLIC KEY",Bytes: x509.MarshalPKCS1PublicKey(pubkey)}))
-    return pubkey_pem
+	pubkey_pem := string(pem.EncodeToMemory(&pem.Block{Type: "RSA PUBLIC KEY", Bytes: x509.MarshalPKCS1PublicKey(pubkey)}))
+	return pubkey_pem
 }
 
 func ExportPrivateKeyAsPemStr(privatekey *rsa.PrivateKey) string {
-    privatekey_pem := string(pem.EncodeToMemory(&pem.Block{Type:  "RSA PRIVATE KEY",Bytes: x509.MarshalPKCS1PrivateKey(privatekey)}))
-    return privatekey_pem
+	privatekey_pem := string(pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privatekey)}))
+	return privatekey_pem
 }
 
 func ExportMsgAsPemStr(msg []byte) string {
-    msg_pem := string(pem.EncodeToMemory(&pem.Block{Type:  "MESSAGE",Bytes: msg}))
-    return msg_pem
+	msg_pem := string(pem.EncodeToMemory(&pem.Block{Type: "MESSAGE", Bytes: msg}))
+	return msg_pem
 }
 
 // BytesToPrivateKey bytes to private key
 func BytesToPrivateKey(priv []byte, passphrase []byte) *rsa.PrivateKey {
-    entityList, err := openpgp.ReadArmoredKeyRing(bytes.NewReader(priv))
+	entityList, err := openpgp.ReadArmoredKeyRing(bytes.NewReader(priv))
 	if err != nil {
-        fmt.Println("Error reading private key: ", err)
+		fmt.Println("Error reading private key: ", err)
 		return nil
 	}
 
-    tamPrivateKey := entityList[0].PrivateKey
+	tamPrivateKey := entityList[0].PrivateKey
 	if tamPrivateKey == nil {
-        fmt.Println("No private key found in armor block")
+		fmt.Println("No private key found in armor block")
 		return nil
 	}
 
 	err = tamPrivateKey.Decrypt(passphrase)
 	if err != nil {
-        fmt.Println("Error decrypting private key: ", err)
+		fmt.Println("Error decrypting private key: ", err)
 		return nil
 	}
 
 	if tamPrivateKey.PubKeyAlgo != packet.PubKeyAlgoRSA {
-        fmt.Println("PGP key is not RSA private key")
+		fmt.Println("PGP key is not RSA private key")
 		return nil
 	}
 
@@ -58,15 +60,15 @@ func BytesToPrivateKey(priv []byte, passphrase []byte) *rsa.PrivateKey {
 func BytesToPublicKey(pub []byte) *rsa.PublicKey {
 	// block, _ := armor.Decode(bytes.NewReader(pub))
 
-    entityList, err := openpgp.ReadArmoredKeyRing(bytes.NewReader(pub))
+	entityList, err := openpgp.ReadArmoredKeyRing(bytes.NewReader(pub))
 	if err != nil {
-        fmt.Println("Error reading public key: ", err)
+		fmt.Println("Error reading public key: ", err)
 		return nil
 	}
 
 	tamPublicKey := entityList[0].PrimaryKey
 	if tamPublicKey == nil {
-        fmt.Println("Error reading public key: ", err)
+		fmt.Println("Error reading public key: ", err)
 		return nil
 	}
 
@@ -77,5 +79,48 @@ func BytesToPublicKey(pub []byte) *rsa.PublicKey {
 	return tamPublicKey.PublicKey.(*rsa.PublicKey)
 }
 
+// Encrypt too long message
+func EncryptOAEP(hash hash.Hash, random io.Reader, public *rsa.PublicKey, msg []byte, label []byte) ([]byte, error) {
+	msgLen := len(msg)
+	step := public.Size() - 2*hash.Size() - 2
+	var encryptedBytes []byte
 
+	for start := 0; start < msgLen; start += step {
+		finish := start + step
+		if finish > msgLen {
+			finish = msgLen
+		}
 
+		encryptedBlockBytes, err := rsa.EncryptOAEP(hash, random, public, msg[start:finish], label)
+		if err != nil {
+			return nil, err
+		}
+
+		encryptedBytes = append(encryptedBytes, encryptedBlockBytes...)
+	}
+
+	return encryptedBytes, nil
+}
+
+// Decrypt too long message
+func DecryptOAEP(hash hash.Hash, random io.Reader, private *rsa.PrivateKey, msg []byte, label []byte) ([]byte, error) {
+    msgLen := len(msg)
+    step := private.PublicKey.Size()
+    var decryptedBytes []byte
+
+    for start := 0; start < msgLen; start += step {
+        finish := start + step
+        if finish > msgLen {
+            finish = msgLen
+        }
+
+        decryptedBlockBytes, err := rsa.DecryptOAEP(hash, random, private, msg[start:finish], label)
+        if err != nil {
+            return nil, err
+        }
+
+        decryptedBytes = append(decryptedBytes, decryptedBlockBytes...)
+    }
+
+    return decryptedBytes, nil
+}
--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"crypto/rand"
-	"crypto/rsa"
+	// "crypto/rsa"
 	"crypto/sha256"
 	"decode-decrypt-playground/ciphers"
+	"decode-decrypt-playground/unzips"
 	"fmt"
 	"io/ioutil"
-
 )
 
 const pubKey = "ciphers/tam-public-key.asc"
@@ -25,6 +25,7 @@ func main() {
  	m := "Test-RSA Decryption with Tam's Private Key"
 	fmt.Printf("\nMessage : %s\n\n", m)
 
+
 	tamPublicKeyData, _ := ioutil.ReadFile("ciphers/tamtam-public-key.asc")
 	tamPrivateKeyData, _ := ioutil.ReadFile("ciphers/tamtam-private-key.asc")
 	phassphrase := []byte("tamtam1122")
@@ -35,12 +36,22 @@ func main() {
  	fmt.Printf("%s\n",  ciphers.ExportPrivateKeyAsPemStr(tamPrivateKey))
  	fmt.Printf("%s\n", ciphers.ExportPublicKeyAsPemStr(tamPublicKey))
 
- 	message := []byte(m)
+ 	// message := []byte(m)
  	label := []byte("")
  	hash := sha256.New()
 
+	fileRaw, _ := ioutil.ReadFile("ciphers/covid-thailand-2021.csv.zip")
 	// Encrypt message with Tam's Public Key
- 	ciphertext, _ := rsa.EncryptOAEP(hash, rand.Reader, tamPublicKey, message, label)
+ 	ciphertext, err := ciphers.EncryptOAEP(hash, rand.Reader, tamPublicKey, fileRaw, label)
+	if err != nil {
+		fmt.Printf("Error Encrypt: %s", err)
+		return
+	}
+	err = ioutil.WriteFile("encrypt-file/covid-thailand-2021.pgp", ciphertext, 0644)
+	 if err != nil {
+		 fmt.Printf("Error %s", err)
+		 return
+	 }
  	fmt.Printf("%s\n",ciphers.ExportMsgAsPemStr(ciphertext))
 
 	// Encode & Decode message from RSA Encryption by use base64
@@ -48,6 +59,24 @@ func main() {
 	ciphers.DecodeString(data)
 
  	// Decrypt message with Tam's Private Key
- 	plainText, _:= rsa.DecryptOAEP(hash, rand.Reader, tamPrivateKey, ciphertext, label)
- 	fmt.Printf("RSA decrypted : %s", plainText)
+	fileEnc, _ := ioutil.ReadFile("encrypt-file/covid-thailand-2021.pgp")
+ 	fileDecrypt, err := ciphers.DecryptOAEP(hash, rand.Reader, tamPrivateKey, fileEnc, label)
+	if err != nil {
+		fmt.Printf("Error Decrypt: %s", err)
+		return
+	}
+	err = ioutil.WriteFile("decrypt-file/covid-thailand-2021.zip", fileDecrypt, 0644)
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+	fmt.Printf("Decrypted success\n")
+ 	// fmt.Printf("RSA decrypted : %s", plainText)
+
+	// Unzip file
+	err = unzips.UnzipSource("decrypt-file/covid-thailand-2021.zip", "csv-files")
+	if err != nil {
+		fmt.Printf("Error unzipfile : %s", err)
+		return
+	}
 }
